bad2899862c66881c6ac020a805924c72ff5e881
Servin Sanchez <93779413+Serv08@users.noreply.github.com>
2023-05-26 23:55:25 +0800

    Changed to a new dataset

--- hello.py
+++ hello.py
@@ -1,16 +1,19 @@
-from pyspark import SparkContext 
+from pyspark import SparkContext, SparkConf
 
 # Create SparkContext instance
-sc = SparkContext(appName="CSV Parallelization")
+# sc = SparkContext(appName="CSV Parallelization")
+sc = SparkContext()
 
 # Load dataset in Python as RDD
 filePath = "Nutritional Facts for Most Common Foods/nutrients_csvfile.csv"
 dataSetRDD = sc.textFile(filePath, minPartitions=10)
-# print(type(dataSetRDD))
+print("Data type is ", type(dataSetRDD))
 
 # Choose (at least) 2 from the basic RDD transformation: map(), filter(), flatMap(), union()
+# mapRDD = map(lambda x:)
 
 # Return value using RDD actions: collect(), take(), first(), count()
+
 
 # If datasets contain  value that groups data together, use: sortByKey(), groupByKey()
 # Then apply setp 2 and 3
